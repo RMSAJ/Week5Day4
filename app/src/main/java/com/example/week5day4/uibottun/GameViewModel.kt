ab425c2745254a5fb7be21e43bb7f997b8d28f07
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-11 01:33:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/week5day4/uibottun/GameViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.week5day4.uibottun
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+private val score = 0
+private   var _curruntScore: Int
+
+
+init {
+    _curruntScore = 0
+}
+
+ fun _plusOne() {
+
+ ++_curruntScore
+
+}
+
+ fun _minusTwo() {
+return if (_curruntScore >= 2 ) { _curruntScore -= 2}
+        else _curruntScore = 0
+}
+
+ fun _plusFour() {
+ _curruntScore += 4
+}
+
+val currentScore get() = _curruntScore
+//val plusOne get() = _plusOne()
+//val minusTwo get() = _minusTwo()
+//val plusfore get() = _plusFour()
+
+
+}
---
+++ app/src/main/java/com/example/week5day4/uibottun/ThreeButtonFragment.kt
@@ -0,0 +1,46 @@
+package com.example.week5day4.uibottun
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.week5day4.R
+import com.example.week5day4.databinding.FragmentThreeButtonBinding
+
+class ThreeButtonFragment : Fragment() {
+private val viewmodel: GameViewModel by viewModels()
+
+    private lateinit var binding: FragmentThreeButtonBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentThreeButtonBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        updateScoreOnScreen()
+        binding.add1.setOnClickListener { viewmodel._plusOne()
+        updateScoreOnScreen()}
+        binding.sub2.setOnClickListener { viewmodel._minusTwo()
+        updateScoreOnScreen()}
+        binding.add4.setOnClickListener { viewmodel._plusFour()
+        updateScoreOnScreen()}
+    }
+
+   private fun updateScoreOnScreen(){
+    binding.displayNum.text = getString(R.string.score,viewmodel.currentScore )
+
+
+   }
+
+
+}
