e7a89759f14580a0d7d2ace7ccb4cf6d1b560187
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-11 20:35:22 +0300

    updated with the last topic

--- app/src/main/java/com/example/week5day4/uibottun/GameViewModel.kt
+++ app/src/main/java/com/example/week5day4/uibottun/GameViewModel.kt
@@ -1,32 +1,33 @@
 package com.example.week5day4.uibottun
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 private val score = 0
-private   var _curruntScore: Int
+private   val _curruntScore = MutableLiveData <Int> (0)
 
 
-init {
-    _curruntScore = 0
-}
+
 
  private fun _plusOne() {
 
- ++_curruntScore
+ _curruntScore.value = _curruntScore.value?.plus(1)
 
 }
 
  private fun _minusTwo() {
-return if (_curruntScore >= 2 ) { _curruntScore -= 2}
-        else _curruntScore = 0
-}
+     return if (_curruntScore.value!! >= 2) {
+         _curruntScore.value = _curruntScore.value!!.minus(2)
+     } else _curruntScore.value = 0
+ }
 
  private fun _plusFour() {
- _curruntScore += 4
+     _curruntScore.value = _curruntScore.value?.plus(4)
 }
 
-val currentScore get() = _curruntScore
+val currentScore: LiveData<Int> get() = _curruntScore
 val plusOne get() = _plusOne()
 val minusTwo get() = _minusTwo()
 val plusfore get() = _plusFour()
--- app/src/main/java/com/example/week5day4/uibottun/ThreeButtonFragment.kt
+++ app/src/main/java/com/example/week5day4/uibottun/ThreeButtonFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
@@ -20,27 +21,18 @@ private val viewmodel: GameViewModel by viewModels()
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentThreeButtonBinding.inflate(inflater,container,false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_three_button, container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        updateScoreOnScreen()
-        binding.add1.setOnClickListener { viewmodel.plusOne
-        updateScoreOnScreen()}
-        binding.sub2.setOnClickListener { viewmodel.minusTwo
-        updateScoreOnScreen()}
-        binding.add4.setOnClickListener { viewmodel.plusfore
-        updateScoreOnScreen()}
+        binding.gameViewModel = viewmodel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.add1.setOnClickListener { viewmodel.plusOne }
+        binding.sub2.setOnClickListener { viewmodel.minusTwo }
+        binding.add4.setOnClickListener { viewmodel.plusfore }
     }
-
-   private fun updateScoreOnScreen() {
-    binding.displayNum.text = getString(R.string.score,viewmodel.currentScore )
-
-
-   }
-
-
 }
